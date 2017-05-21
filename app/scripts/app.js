b094cc7675180582bf58e569c2a4f9749c12af85
ANDREW NEIDLEY <andrewneidley@ANDREWs-MacBook-Pro.local>
2017-05-21 18:20:05 -0500

    Checkpoint 3 edit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,7 @@
             .state('landing', {
                 url:'/',
                 templateUrl: '/templates/landing.html'
+            })
             .state('album', {
                 url:'/album',
                 templateUrl: '/templates/album.html'
