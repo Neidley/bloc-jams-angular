ae10fe64f5375a37900c74fed0ca2f750a960696
ANDREW NEIDLEY <andrewneidley@ANDREWs-MacBook-Pro.local>
2017-05-24 22:24:31 -0500

    checkpoint 7-services-part 2: in progress

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -9,11 +9,11 @@
             year: '1881',
             albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-                { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-                { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-                { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-                { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-                { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+                { title: 'Blue', duration: '161.71', audioUrl: '/assets/music/blue' },
+                { title: 'Green', duration: '103.96', audioUrl: '/assets/music/green' },
+                { title: 'Red', duration: '268.45', audioUrl: '/assets/music/red' },
+                { title: 'Pink', duration: '153.14', audioUrl: '/assets/music/pink' },
+                { title: 'Magenta', duration: '374.22', audioUrl: '/assets/music/magenta' }
             ]
         };
 
@@ -26,9 +26,9 @@
             songs: [
                 { title: 'Hello, Operator?', duration: '1:01' },
                 { title: 'Ring, ring, ring', duration: '5:01' },
-                { title: 'Fits in your pocket', duration: '3:21'},
+                { title: 'Fits in your pocket', duration: '3:21' },
                 { title: 'Can you hear me now?', duration: '3:14' },
-                { title: 'Wrong phone number', duration: '2:15'}
+                { title: 'Wrong phone number', duration: '2:15' }
             ]
         };
 
